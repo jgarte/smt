597940e86f06f9945de2c269719586cbf8f22bae
Amir <ateymuri63@gmail.com>
2021-04-17 00:59:09 +0200

    parser skizze

--- lang.py
+++ lang.py
@@ -38,18 +38,138 @@ vs. anonymus function:
 
 [function [x y] a doc string? [print [list x y]]]
 
-[procedure make_note [p] [note [pitch p] [duration 1]]
+[procedure make_note [p] [note [pitch p] [duration 1] [id foo]]
 [line [toplevel yes] [make_note c4]]
 
+comment whole line, aber das wird trotzdem gemach [+ 2 1]
+[print [getby pitch c4] 
+[getby id 
+foo]
+]
 
 
 
-
-
+[fun FoBar]
 
 
 """
-
+# import inspect
+# import re
+import rply
 from score import *
 
-print(Stem, Clef, HForm, Char)
+
+lg = rply.LexerGenerator()
+
+# lg.add('NUMBER', r'\d+')
+lg.add('NUMBER', r'[\d\s]+')
+lg.add('PLUS', r'\+')
+lg.add('OPEN', r'\[')
+lg.add('CLOSE', r'\]')
+
+lg.ignore('\s+')
+
+lexer = lg.build()
+
+
+class Number(rply.token.BaseBox):
+    def __init__(self, value):
+        self.value = value
+    def eval(self):
+        return self.value
+class Numbers(rply.token.BaseBox):
+    def __init__(self, *value):
+        self.value = value
+    def eval(self):
+        return self.value
+
+# class Prefix(rply.token.BaseBox):
+    # def __init__(self, cdr):
+        # # self.left = left
+        # self.cdr = cdr
+
+class Add(rply.token.BaseBox):
+   def __init__(self, cdr):
+      self.cdr =cdr
+   def eval(self):
+      # print("CDR",self.cdr)
+      return sum(self.cdr[0].eval())
+      # return self.cdr.eval()+.2
+
+
+pg = rply.ParserGenerator(
+    # A list of all token names, accepted by the parser.
+    ['NUMBER', 'OPEN', 'CLOSE',
+     'PLUS'
+    ],
+    # A list of precedence rules with ascending precedence, to
+    # disambiguate ambiguous production rules.
+    # precedence=[
+        # ('left', ['PLUS', 'MINUS']),
+        # ('left', ['MUL', 'DIV'])
+    # ]
+)
+
+@pg.production('expression : NUMBER')
+def expression_number_____(p):
+    # p is a list of the pieces matched by the right hand side of the
+    # rule
+    # print("---------",p[0].getstr().split(" "))
+    # print("==========",Number([int(x) for x in p[0].getstr().split(" ")]).eval())
+    return(Number([int(x) for x in p[0].getstr().split(" ")]))
+    # return Number(int(p[0].getstr()))
+
+# @pg.production('expression : OPEN expression CLOSE')
+# def expression_parens(p):
+   # print(">>>>", p)
+   # return p[1]
+
+@pg.production('expression : OPEN PLUS expression CLOSE')
+# @pg.production('expression : expression MINUS expression')
+# @pg.production('expression : expression MUL expression')
+# @pg.production('expression : expression DIV expression')
+def expression_binop(p):
+    # left = p[0]
+    # right = p[2]
+   # print(">>",p[1].gettokentype())
+   if p[1].gettokentype() == 'PLUS':
+      return Add(p[2:-1])
+   else:
+      raise AssertionError('Oops, this should not be possible!')
+
+parser = pg.build()
+
+print(parser.parse(lexer.lex('[+ 102 3 45]')).eval())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+smtobjs = {
+   "hform": HForm, "sform": SForm, "vform": VForm, "char": Char,
+   "note": Note
+}
+smt_pubattrs = {
+   "note": [x for x in dir(Note) if not x.startswith("_") and not callable(x)] 
+}
+
+
+
+
+# print(pubattrs["note"], not callable(Note.addsvg))
+# print([x for x in dir(Note) if not x.startswith("_") and not inspect.isfunction(x)])
+# pattern = "\[[\w]\]"
+# print(re.fullmatch(pattern, "[note [pitch [+1 3]]]"))
--- pad.py
+++ pad.py
@@ -54,20 +54,22 @@ def save_file():
         output_file.write(text)
     window.title(f"{_CAPTION} - {filepath}")
 
-window = tk.Tk()
-window.title(_CAPTION)
-window.rowconfigure(0, minsize=400, weight=1)
-window.columnconfigure(1, minsize=300, weight=1)
 
-txt_edit = tk.Text(window)
-fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
-btn_open = tk.Button(fr_buttons, text="Open", command=open_file)
-btn_save = tk.Button(fr_buttons, text="Save As... (R)", command=save_file)
 
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+# window = tk.Tk()
+# window.title(_CAPTION)
+# window.rowconfigure(0, minsize=400, weight=1)
+# window.columnconfigure(1, minsize=300, weight=1)
 
-fr_buttons.grid(row=0, column=0, sticky="ns")
-txt_edit.grid(row=0, column=1, sticky="nsew")
+# txt_edit = tk.Text(window)
+# fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+# btn_open = tk.Button(fr_buttons, text="Open", command=open_file)
+# btn_save = tk.Button(fr_buttons, text="Save As... (R)", command=save_file)
 
-window.mainloop()
+# btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+# btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+
+# fr_buttons.grid(row=0, column=0, sticky="ns")
+# txt_edit.grid(row=0, column=1, sticky="nsew")
+
+# window.mainloop()
