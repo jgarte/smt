a71303f0a02673732b9b4f0d733a24256b7072cb
Amir <ateymuri63@gmail.com>
2021-05-23 00:08:21 +0200

    first prototype done

--- lang2.py
+++ lang2.py
@@ -1,143 +1,133 @@
 
 import re
-
-"""
-+
-  2 3 4
-  *
-    10 10 + 
-            20 20
-            30 30
-fn
-  p1
-    p2
-  + p1 p2
-  * p1 p2
-  + p1 * p2 100
-
-  
-arglose fn?
-
-call
-  fn
-    append list 2 3 4
-        list 1 2 3
-
--> ([2,3,4], [1,2,3])
-
-call
-  fn
-    p1 p2
-      p3
-    append list 2 3 4
-           list p1 p2 p3
-  1 2 3
--> [2,3,4,1,2,3]
-
-defn name
-  p1 p2
-     p3 p4
-     p5 p6
-  + p1 p2 p3 p4 p5 p6
-
-name 3 + 5 6
-         4 5
-
-case
-  = * 10 10 10
-    + 10 2 - 9 8
-             23
-  print "Hello you?"
-  true
-  print "No!"
-"""
-
-
-
-
-
-
+from functools import reduce
 
 
 
 s="""
 + 1 2 3 4
-  * 3 4 5 + 6 7
-  8 9 10 1112 0
+  * 3 4 5 + 6 7 * 11 23      + 1000 1000
+  8 9 10 1112 + 0 0 230  * 3 4    500
+  1000            500 400 300
 """
-# Blocks at specific indentation levels!!!!!!!!!!!!!!!!!!!!111111
-kwenv = {
-    "*": None,
-    "+": None
+
+funenv = {
+    "*": lambda *args: reduce(lambda x, y: x*y, args),
+    "+": lambda *args: sum(args)
 }
-def iskw(s): return s in kwenv
+
+class Token:
+    def __init__(self, label, start, end, line):
+        self.label = label
+        self.start = start
+        self.end = end
+        self.line = line
+        self.allocated = False
+        self.value = None
+    
+    def __repr__(self):
+        return f"{self.label}"
+
+def resolve_token(t):
+    try:
+        return int(t.label)
+    except ValueError:
+        try:
+            return float(t.label)
+        except ValueError:
+            try:
+                return funenv[t.label]
+            except KeyError:
+                pass
+
+def set_token_value_ip(t):
+    try:
+        t.value = int(t.label)
+    except ValueError:
+        try:
+            t.value = float(t.label)
+        except ValueError:
+            try:
+                t.value = funenv[t.label]
+            except KeyError:
+                pass
+
+    
+
+def tokisfun(t): return t.label in funenv
+
 def lines(src): return src.strip().splitlines()
+
 def tokenize_source(src):
     toks = []
-    idx = 0
     for i, line in enumerate(lines(src)):
-        for match in re.finditer("[*+\w\d]+", line):
-            toks.append({
-                "str": match.group(), "start": match.start(),
-                "end": match.end(), "line": i, "idx": idx,
-                "alloced": False
-            })
-            idx += 1
+        for match in re.finditer("[*+\w\d]+", line):    
+            toks.append(Token(label=match.group(), start=match.start(), end=match.end(), line=i)
+            # {
+                # "str": match.group(), "start": match.start(),
+                # "end": match.end(), "line": i, "idx": idx,
+                # "alloced": False, "obj": None
+            # }
+            )
     return toks
 
 def tokensatline(line, toks):
-    return [t for t in toks if t["line"] == line]
+    return [t for t in toks if t.line == line]
 
-def kwatline(line, tokens):
-    return [t for t in tokens if iskw(t["token"]) and t["line"] == line]
-
-toks = tokenize_source(s)
 
 
 def linekws(linetoks):
     """Returns a list of kw tokens at current line."""
-    return [t for t in linetoks if iskw(t["str"])]
+    return [t for t in linetoks if tokisfun(t)]
 
-def isinblock(tok, kw):
-    """Is token inside of the kw's block?
-    
+def is_atomic_subordinate(tok, kw):
+    """Is token inside of the kw's block, eg an arg etc
     """
-    return (not iskw(tok["str"])) and tok["start"] > kw["start"] and \
-    tok["line"] >= kw["line"] and (not tok["alloced"])
+    return (not tokisfun(tok)) and tok.start > kw.start and \
+    tok.line >= kw.line and (not tok.allocated)
 
-def allocate_literals(toks):
+def allocate_atomics(toks):
+    """
+    [operator . . . . . . . .]
+    """
     L = []
-    maxline = max(toks, key=lambda t: t["line"])["line"]
+    maxline = max(toks, key=lambda t: t.line).line
     for i in range(maxline, -1, -1):
         kwtoks = linekws(tokensatline(i, toks))
         if kwtoks:
-            for kw in sorted(kwtoks, key=lambda t: t["start"], reverse=True):
+            for kw in sorted(kwtoks, key=lambda t: t.start, reverse=True):
+                set_token_value_ip(kw)
                 x=[kw]
-                for a in [t for t in toks if isinblock(t, kw)]:
+                for a in [t for t in toks if is_atomic_subordinate(t, kw)]:
+                    a.allocated = True
+                    set_token_value_ip(a)
+                    # x.append(resolve_token(a))
+                    # resolve_token(a)
                     x.append(a)
-                    a["alloced"] = True
                 L.append(x)
     return L
 
-for b in allocate_literals(toks):
-    print([x["str"] for x in b])
-
-# for b in blocks(toks):
-    # print([t["token"] for t in b])
-
-# for t in toks:
-    # print(t, iskw(t["str"]))
-
-# print(kwatline(0, spans))
+def ast(atomic_lists):
+    while len(atomic_lists) > 1:
+        lst = atomic_lists.pop(0)
+        for x in atomic_lists:
+            if x[0].start < lst[0].start and x[0].line <= lst[0].line:
+                x.append(lst)
+                break
+    return atomic_lists[0]
 
 
+def evalexp(x):
+    try: # a token obj?
+        return x.value
+    except AttributeError: # a list?
+        try:
+            fn, *args = x
+            return evalexp(fn)(*[evalexp(a) for a in args])
+        except KeyError:
+            pass
 
-
-
-
-
-
-
-
-
-
+toks = tokenize_source(s)
+print(evalexp(ast(allocate_atomics(tokenize_source(s)))))
+# for x in allocate_atomics(tokenize_source(s)):
+    # print([a.label for a in x])
